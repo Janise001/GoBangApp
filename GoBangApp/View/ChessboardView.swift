0a46f8218d06823b9e4d582ee6866aa31f656031
吴丽娟 <wlj@zjmax.com>
2018-09-18 18:13:59 +0800

    no message

---
+++ GoBangApp/View/ChessboardView.swift
@@ -0,0 +1,29 @@
+//
+//  ChessboardView.swift
+//  GoBangApp
+//
+//  Created by 吴丽娟 on 2018/9/18.
+//  Copyright © 2018年 Janise. All rights reserved.
+//
+
+import UIKit
+
+class ChessboardView: UIView {
+
+
+
+}
+public enum DirectionType {
+    case horizontal
+    case vertical
+}
+open class LineView:CALayer {
+    
+//    public convenience init(directionType:DirectionType = .horizontal,size:CGFloat = 1) {
+//        self.init(frame: CGRect())
+//
+//    }
+    open override func draw(in ctx: CGContext) {
+//        ctx.add
+    }
+}
